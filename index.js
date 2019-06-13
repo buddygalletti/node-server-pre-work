b380b02d108871a525e5823265563b244f101b50
Buddy Galletti <buddygalletti@gmail.com>
2019-06-13 08:19:53 -0700

    Some req.url logic

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const http = require('http');
+const flavors = require('./flavors');
 
 const server = http.createServer((req, res) => {
-    res.write('Hello World!!`')
-    res.end()
+  if (req.url === '/flavors') {
+    res.write(`The flavors are ${flavors.join(', ')}.`)
+  } else {
+    res.write('Hello World!');
+  }
+  res.end();
 });
 server.listen(3000);
