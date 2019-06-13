1adddc2fc971c108f00c0faa87e92b10699fd078
Buddy Galletti <buddygalletti@gmail.com>
2019-06-13 08:23:51 -0700

    req methods

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const http = require('http');
 const flavors = require('./flavors');
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(function(req, res) {
   if (req.url === '/flavors') {
-    res.write(`The flavors are ${flavors.join(', ')}.`)
+    if (req.method === 'POST') {
+      res.write("Error: You can't add another flavor.")
+    } else {
+      res.write(`The flavors are ${flavors.join(', ')}.`)
+    }
   } else {
     res.write('Hello World!');
   }
