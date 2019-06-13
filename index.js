94d2898d448a4ae2da789bac71c131fa5a944325
Buddy Galletti <buddygalletti@gmail.com>
2019-06-13 08:12:48 -0700

    Created a server

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.write('Hello World!!')
+    res.write('Hello World!!`')
     res.end()
 });
 server.listen(3000);
